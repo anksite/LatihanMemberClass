1e95efb2ccf882e77511d6a01567fe32eb9af99c
anksite <adnankurniawan98@gmail.com>
2019-03-26 14:51:51 +0700

    commit awal

---
+++ LatihanMemberClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Taxi taxi = new Taxi();
+            taxi.driverName = "Jono";
+            taxi.onDuty = false;
+            taxi.numPassenger = 10;
+
+            taxi.taxiInfo();
+            Console.WriteLine();
+            taxi.pickUpPassenger();
+            taxi.dropOfPassenger();
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ LatihanMemberClass/Taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass {
+
+    class Taxi {
+        public string driverName    { get; set; }
+        public Boolean onDuty       { get; set; }
+        public int numPassenger     { get; set; }
+
+        public void taxiInfo() {
+            Console.WriteLine("Driver Name: {0}", driverName);
+
+            Console.WriteLine("On Duty: {0}", onDuty ? "Yes" : "No");
+
+            //if (onDuty) {
+            //    Console.WriteLine("On Duty: Yes");
+            //} else {
+            //    Console.WriteLine("On Duty: No");
+            //}
+
+            Console.WriteLine("Num Passenger: {0}", numPassenger);
+        }
+
+        public void pickUpPassenger() {
+            Console.WriteLine("{0} sedang menjemput penumpang", driverName);
+        }
+
+        public void dropOfPassenger() {
+            Console.WriteLine("{0} sedang mengantar penumpang", driverName);
+        }
+
+    }
+}
